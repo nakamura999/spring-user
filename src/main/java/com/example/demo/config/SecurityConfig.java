25772f332e6e8818336cd5a09de3ee83f418448d
nakamura999 <masahiro9ama9@gmail.com>
2020-08-24 15:35:07 +0900

    security edit

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -7,13 +7,21 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
+import com.example.demo.util.Role;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 @Configuration
 @EnableWebSecurity
 // ↑Spring Securityの機能を有効にする
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	private final UserDetailsService userDetailsService;
+
 	@Bean
 	public BCryptPasswordEncoder passwordEncoder() {
 		// パスワードの暗号化にbcryptを使用
@@ -24,14 +32,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	public void configure(WebSecurity web) throws Exception {
 		// セキュリティ設定を無視(ignoring)するパスを指定
 		// css・js・imgなどの静的ソースを指定
-		web.ignoring().antMatchers("/css/**", "/webjars/**");
+		web.ignoring().antMatchers("/js/**", "/css/**", "/webjars/**");
 	}
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
 				// ログインをアクセス可能にする
-				.antMatchers("/login").permitAll()
+				.antMatchers("/login", "/register").permitAll()
+				// /admin は、ADMINユーザーだけアクセス可能にする
+				.antMatchers("/admin/**").hasRole(Role.ADMIN.name())
 				.anyRequest().authenticated()
 				.and()
 			.formLogin()
@@ -51,15 +61,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication()
-			// ユーザー名 admin or user
-			// パスワード両方ともpassword
-			.withUser("admin")
-				.password(passwordEncoder().encode("12345678"))
-				.authorities("ROLE_ADMIN")
-				.and()
-			.withUser("user")
-				.password(passwordEncoder().encode("12345678"))
-				.authorities("ROLE_USER");
+		auth.userDetailsService(userDetailsService)
+			.passwordEncoder(passwordEncoder());
+//		auth.inMemoryAuthentication()
+//			 ユーザー名 admin or user
+//			 パスワード両方ともpassword
+//			.withUser("admin")
+//				.password(passwordEncoder().encode("12345678"))
+//				.authorities("ROLE_ADMIN")
+//				.and()
+//			.withUser("user")
+//				.password(passwordEncoder().encode("12345678"))
+//				.authorities("ROLE_USER");
 	}
 }
