172086e42e78e8053a760799a27a12c9dc80eb29
nakamura999 <masahiro9ama9@gmail.com>
2020-08-26 16:34:48 +0900

    js edit

---
+++ src/main/resources/static/js/usertables.js
@@ -0,0 +1,16 @@
+/**
+ *
+ */
+ $(function(){
+	$("#user-table").dataTable({
+		// DataTablesを日本語化
+		"language": {"url": "/webjars/datatables-plugins/i18n/Japanese.json"},
+		//各種ボタンを有効化する
+		dom: 'Bfrtip',
+		buttons: [
+			'excelHtml5',
+			'csvHtml5',
+			'print'
+		]
+	});
+});
