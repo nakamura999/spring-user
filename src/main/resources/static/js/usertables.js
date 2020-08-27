7ecec70b597f7edfcee3a535b41c8ce60e368777
nakamura999 <masahiro9ama9@gmail.com>
2020-08-27 16:26:27 +0900

    error edit

--- src/main/resources/static/js/usertables.js
+++ src/main/resources/static/js/usertables.js
@@ -1,16 +1,13 @@
-/**
- *
- */
  $(function(){
 	$("#user-table").dataTable({
 		// DataTablesを日本語化
-		"language": {"url": "/webjars/datatables-plugins/i18n/Japanese.json"},
+		"language": {
+			"url": "/webjars/datatables-plugins/i18n/Japanese.json"
+		},
 		//各種ボタンを有効化する
 		dom: 'Bfrtip',
 		buttons: [
-			'excelHtml5',
-			'csvHtml5',
-			'print'
+			'excelHtml5', 'csvHtml5', 'print'
 		]
 	});
 });
